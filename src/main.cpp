786ab38585e4ee948659d12d66c7b80145370792
RoboPesce <stlecorno@gmail.com>
2023-01-10 20:15:01 +0000

    trying to debug

--- src/main.cpp
+++ src/main.cpp
@@ -61,9 +61,13 @@ int main(int argc, char** argv)
     for(int i = 0; i < train_size; i++) 
     {
         cout << "for point " << training_data[i] << " (" << desired_outputs[i] << ')' << endl;
-        cout << "loss before: " << NeuralNetwork::calculateLoss(nn.predict(training_data[i]), desired_outputs[i]) << endl;
+        vector<double> prediction = nn.predict(training_data[i]);
+        cout << "prediction before: " << prediction << endl;
+        cout << "loss before: " << NeuralNetwork::calculateLoss(prediction, desired_outputs[i]) << endl;
         nn.backpropagation(training_data[i], desired_outputs[i], learning_rate);
-        cout << "loss after: " << NeuralNetwork::calculateLoss(nn.predict(training_data[i]), desired_outputs[i]) << endl;
+        prediction = nn.predict(training_data[i]);
+        cout << "prediction after: " << prediction << endl;
+        cout << "loss after: " << NeuralNetwork::calculateLoss(prediction, desired_outputs[i]) << endl;
     }
 
     cout << "\nNetwork after: \n" << nn << endl;
